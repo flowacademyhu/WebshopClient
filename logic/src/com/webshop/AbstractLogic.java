16d89d7e37de0d0c5c63a36f9895028e393156bf
Godfrid <godfrid@gmail.com>
2018-08-16 12:04:46 +0200

    AbstractLogic, Logic

--- logic/src/com/webshop/AbstractLogic.java
+++ logic/src/com/webshop/AbstractLogic.java
@@ -1,5 +1,39 @@
 package com.webshop;
 
-public abstract class AbstractLogic {
+import com.webshop.factory.GatewayFactory;
+import com.webshop.factory.PluginFactory;
+import com.webshop.interactor.SessionManager;
 
+public abstract class AbstractLogic {
+    private PluginFactory pluginFactory;
+    private GatewayFactory gatewayFactory;
+
+    private GatewayFactory getGatewayFactory() {
+        if (gatewayFactory == null) {
+            gatewayFactory = createGatewayFactory();
+        }
+        return gatewayFactory;
+    }
+
+    protected abstract GatewayFactory createGatewayFactory();
+
+    public PluginFactory getPluginFactory() {
+        if (pluginFactory == null) {
+            pluginFactory = createPluginFactory();
+        }
+        return pluginFactory;
+    }
+
+    protected abstract PluginFactory createPluginFactory();
+
+
+
+    private SessionManager sessionManager;
+
+    public SessionManager getSessionManager() {
+        if (sessionManager == null) {
+            sessionManager = new SessionManager(getGatewayFactory().getAuthenticationGateway());
+        }
+        return sessionManager;
+    }
 }
